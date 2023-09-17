779e98171ae784fc88e8322ea43b4bc8db675c02
Ji Hoon Yang <77009829+gimokddi@users.noreply.github.com>
2023-09-17 21:06:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,99 +1,143 @@
 
 class Register:
     def __init__(self):
-        self.cur_st = None
-        self.nxt_st = None
+        self._cur_st = None
+        self._nxt_st = None
     
+    # sets the next state of the register with a value
     def set(self, value):
-        self.nxt_st = value
+        self._nxt_st = value
 
+    # gets the current state of the register
     def get(self):
-        return self.cur_st
+        return self._cur_st
+    
+    # transfers the next state to the current state
+    def transfer(self):
+        self._cur_st = self._nxt_st
+
+class Port:
+    def __init__(self):
+        self._link_st = None
+    
+    def get(self):
+        if isinstance(self._link_st ,Port):
+            # st dbg
+            print("Port")
+            # ed ddg
+            self._link_st.get()
+        elif isinstance(self._link_st, Register):
+            # st dbg
+            print("Register")
+            # ed ddg            
+            self._link_st.get()
+        else:
+            return self._link_st
+        
+    
+    def link(self, element):
+        self._link_st = element
 
 class ProcessingUnit:
     def __init__(self):
-        self.act_reg = Register()
-        self.act_in  = Register()   # input port
-        self.act_out = Register()   # output port
-        self.wgt_reg     = Register()
-        self.psum_reg    = Register()
-        self.psum_in = Register()
-        self.psum_out = Register()
-        self.link    = None
+        self._act_reg  = Register()
+        self._act_in   = Port()
+        self._act_out  = Port()
         
-    def multiply(self, activation):
-        pass
-    
     def run_cycle(self):
-        self.act_reg.set(self.act_in.get())
-        
-        if self.link != None:
-            self.link.act_in.set(self.act_out.get())
+        self._act_reg.set(self._act_in.get())
 
     def rising_edge(self):
-        self.act_reg.cur_st  = self.act_reg.nxt_st
-        self.act_out.cur_st  = self.act_reg.nxt_st
-        self.psum_reg.cur_st = self.psum_reg.nxt_st
-        self.psum_out.cur_st = self.psum_reg.nxt_st
-
-    def link_pe(self, pe):
-        self.link = pe
+        self._act_reg.transfer()
 
 class SystolicArray:
-    def __init__(self, activation, weight):
+    def __init__(self, activation):
         self.systolic_array = None
         self.activation     = activation                
-        self.wgt            = weight
         self.act_row        = len(activation)
-        pass
     
-    def creator(self):
+    def create(self):
         # makes a systolic array
         self.systolic_array = [ProcessingUnit() for _ in range(self.act_row)]
         
-        # initializes the array with the weights and links        
+        # links each ports with others
         for i in range(self.act_row):
-            self.systolic_array[i].wgt.cur_st = self.wgt[i]
+            if i != 0:
+                self.systolic_array[i]._act_in.link(self.systolic_array[i-1]._act_out)            
+            self.systolic_array[i]._act_out.link(self.systolic_array[i]._act_reg)
             
-            if i < self.act_row-1:
-                self.systolic_array[i].link_pe(self.systolic_array[i+1])
             
-            
-    def simulator(self):
-        self.creator()
+    def simulate(self):
+        self.create()
         act_cnt = 0
+        cnt = 0
         
-        while self.systolic_array[self.act_row-1].act_out.cur_st != self.activation[self.act_row-1]:
+        for i in range(10):
+            if i < len(activation):
+                self.systolic_array[0]._act_in.link(activation[i])
+                print("#{}\t act_in {}".format(i, self.systolic_array[0]._act_in.get()))
+            else:
+                self.systolic_array[0]._act_in.link(None)
+                print("#{}\t act_in {}".format(i, self.systolic_array[0]._act_in.get()))
+        
+        
+                
+        # self.systolic_array[self.act_row-1]._act_reg._cur_st != self.activation[self.act_row-1]
+        
+        while cnt != 10:
+            cnt += 1
+            
             if act_cnt < len(activation):
-                self.systolic_array[0].act_in.set(activation[act_cnt])
+                self.systolic_array[0]._act_in.link(activation[act_cnt])
                 act_cnt += 1
             else :
-                self.systolic_array[0].act_in.set(None)
+                self.systolic_array[0]._act_in.link(None)
                 
+            # st dbg
+            print("after rising edge")
+            # ed dbg
+            
             for i in range(len(self.systolic_array)):
                 self.systolic_array[i].rising_edge()
                 
-                # st dbg
-                print("after rising edge, re{}, act_in_nxt:{}, act_in_cur:{}, act_reg_nxt:{}, act_reg_cur:{}, act_out_nxt:{}, act_out_cur:{}".format(i, self.systolic_array[i].act_in.nxt_st,
-                    self.systolic_array[i].act_in.cur_st, self.systolic_array[i].act_reg.nxt_st, self.systolic_array[i].act_reg.cur_st, self.systolic_array[i].act_out.nxt_st, self.systolic_array[i].act_out.cur_st))
-                # ed dbg
+            # st dbg
+                print("#{}\treg_nxt {}\treg_cur {}".format(i, self.systolic_array[i]._act_reg._nxt_st, self.systolic_array[i]._act_reg._cur_st))                
+            print()
+                                    
+            print("after clock cylce")
+            # ed dbg
             
             for i in range(len(self.systolic_array)):
                 self.systolic_array[i].run_cycle()
 
             # st dbg        
             for i in range(len(self.systolic_array)):            
-                print("after cycle, re{}, act_in_nxt:{}, act_in_cur:{}, act_reg_nxt:{}, act_reg_cur:{}, act_out_nxt:{}, act_out_cur:{}".format(i, self.systolic_array[i].act_in.nxt_st,
-                    self.systolic_array[i].act_in.cur_st, self.systolic_array[i].act_reg.nxt_st, self.systolic_array[i].act_reg.cur_st, self.systolic_array[i].act_out.nxt_st, self.systolic_array[i].act_out.cur_st))
+                print("#{}\treg_nxt {}\treg_cur {}".format(i, self.systolic_array[i]._act_reg._nxt_st, self.systolic_array[i]._act_reg._cur_st))
             print()
-            # ed dbg        
-        
-        
-    
-        
+            # ed dbg
+        print(cnt)    
 
 if __name__ == "__main__":
     activation     = [1, 2, 3, 4]
     weight         = [1, 2, 3, 4]
-    systolic_array = SystolicArray(activation, weight)
-    systolic_array.simulator()
+    systolic_array = SystolicArray(activation)
+    systolic_array.create()
+    # systolic_array.simulate()
+    
+    # for i in range(len(activation)):
+    #     print("#{}\tin_pt {}\tout_pt {}".format(i, type(systolic_array.systolic_array[i]._act_in._link_st), type(systolic_array.systolic_array[i]._act_out._link_st)))
+    
+    processing_element = ProcessingUnit()
+    input_port = Port()
+    output_port = Port()
+    
+    output_port.link(processing_element._act_reg)
+    input_port.link(output_port)
+    
+    processing_element._act_reg._cur_st = 100
+    
+    print(output_port._link_st._cur_st)
+    
+        
+        
+    
